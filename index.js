305481547a7be52b746cb705037a49aad38cd224
Michael Johnson <michaelbojohnson@gmail.com>
2022-03-17 19:27:10 -0500

    commit tree 04b0f27c0fad1c2c76ab316f35f554f1c2d929dd

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const body = document.getElementsByTagName("body")[0];
+
+body.removeChild(main);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent="YOUR-NAME is the champion"
+document.body.append(h1);
